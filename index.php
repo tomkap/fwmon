9102d464d93bf93a630de1873f167a61ab9de02f
niemal <niemal@gmx.com>
2015-11-29 21:59:08 +0200

    Major improvement, added unlimited dynamic table support

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
 <?php
-
-require('routeros_api.class.php');
 require('resources.class.php');
 
-$API = new routerosAPI();
-$API->debug = false;
-
-$config = json_decode(file_get_contents('./config.json'), true);
-
-if (!$API->connect($config['hostname'], $config['username'], $config['password'])) {
-	die('API connection failed, aborting...');
-}
-
-$results = $API->comm("/ip/firewall/$_GET[table]/print");
-$resources = $API->comm("/system/resource/print");
-$resources = $resources['0']; // <- nice api
-
-$mem_perc = round(($resources['free-memory']/$resources['total-memory']) * 100);
-$hdd_perc = round(($resources['free-hdd-space']/$resources['total-hdd-space']) * 100);
-
-$API->disconnect();
+$fwmon = new resources($_GET['table']);
 
 ?><!DOCTYPE html><html>
 <head>
@@ -37,80 +19,38 @@ $API->disconnect();
 			</div>
 
 			<div class="modal-body">
-				<p><b>Platform:</b> <?php echo $resources['platform'] ?></p>
-				<p><b>Model:</b> <?php echo $resources['board-name'] ?></p>
-				<p><b>Version:</b> <?php echo $resources['version'] ?></p>
-				<p><b>CPU:</b><?php echo $resources['cpu'] . ' @ ' . $resources['cpu-frequency'] . 'MHz'; ?></p>
+				<?php
+					echo $fwmon->info['platform'];
+					echo $fwmon->info['model'];
+					echo $fwmon->info['version'];
+					echo $fwmon->info['cpu'];
+				?>
 			</div>
 
 			<div class="modal-footer">
-				<p><b>Uptime:</b> <?php echo $resources['uptime']; ?></p>
-
-				<b class="progress-header">CPU (<font size="4em"><?php echo $resources['cpu-load']; ?>%</font>)</b>
-				<div class="progress progress-striped">
-					<div class="progress-bar <?php echo getProgressClass($resources['cpu-load'], false); ?>" style="width: <?php echo $resources['cpu-load']; ?>%"></div>
-				</div>
-
-				<b class="progress-header">Free memory (<font size="4em"><?php echo $mem_perc; ?>%</font>, <?php echo $resources['free-memory']; ?>KB)</b>
-				<div class="progress progress-striped">
-					<div class="progress-bar <?php echo getProgressClass($mem_perc, true); ?>" style="width: <?php echo $mem_perc; ?>%"></div>
-				</div>
-
-				<b class="progress-header">Free disk (<font size="4em"><?php echo $hdd_perc; ?>%</font>, <?php echo $resources['free-hdd-space']; ?>KB)</b>
-				<div class="progress progress-striped">
-					<div class="progress-bar <?php echo getProgressClass($hdd_perc, true); ?>" style="width: <?php echo $hdd_perc; ?>%"></div>
-				</div>
-
-				<!--
-				<b class="progress-header">Disk bad blocks (<font size="4em"><?php echo $resources['bad-blocks']; ?>%</font>)</b>
-				<div class="progress progress-striped">
-					<div class="progress-bar <?php echo getProgressClass($resources['bad-blocks'], false); ?>" style="width: <?php echo $resources['bad-blocks']; ?>%"></div>
-				</div>
-				-->
+				<?php
+					echo $fwmon->info['uptime'];
+					echo $fwmon->info['cpu-load'];
+					echo $fwmon->info['free-memory'];
+					echo $fwmon->info['free-hdd-space'];
+				?>
 
 				<div class="btn-group btn-group-justified">
 					<a href="./?table=nat" class="btn btn-default">NAT</a>
 					<a href="./?table=filter" class="btn btn-default">FILTER</a>
+					<a href="./?table=mangle" class="btn btn-default">MANGLE</a>
+				</div>
+				<div class="btn-group btn-group-justified">
+					<a href="./?table=connection" class="btn btn-default">CONN</a>
+					<a href="./?table=layer7-protocol" class="btn btn-default">L7-PROT</a>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<table class="table table-striped table-hover">
-		<thead>
-			<tr>
-				<th>CHAIN</th>
-				<th>ACTION</th>
-				<th>PROTO</th>
-				<th>L7_PROTO</th>
-				<th>SRC_ADDR</th>
-				<th>DST_ADDR</th>
-				<th>IN_INT</th>
-				<th>OUT_INT</th>
-				<th>SRC_PORT</th>
-				<th>DST_PORT</th>
-				<th>BTS</th>
-				<th>PKTS</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($results as $result) { ?>
-			<tr title="" data-original-title="" type="button" data-container="body" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="<?php echo $result['comment']; ?>">
-				<td><?php echo $result['chain']; ?></td>
-				<td><?php echo $result['action']; ?></td>
-				<td><?php echo $result['protocol']; ?></td>
-				<td><?php echo $result['layer7-protocol']; ?></td>
-				<td><?php echo $result['src-address']; ?></td>
-				<td><?php echo $result['dst-address']; ?></td>
-				<td><?php echo $result['in-interface']; ?></td>
-				<td><?php echo $result['out-interface']; ?></td>
-				<td><?php echo $result['src-port']; ?></td>
-				<td><?php echo $result['dst-port']; ?></td>
-				<td><?php echo $result['bytes']; ?></td>
-				<td><?php echo $result['packets']; ?></td>
-			</tr>
-			<?php } ?>
-		</tbody>
+		<thead><?php echo $fwmon->t_head; ?></thead>
+		<tbody><?php echo $fwmon->t_rows; ?></tbody>
 	</table>
 
 	<script src="js/jquery-1.10.2.min.js"></script>
--- resources.class.php
+++ resources.class.php
@@ -1,17 +1,163 @@
 <?php
+require_once('routeros_api.class.php');
 
-function getProgressClass($percentage, $rev) {
-	if ($rev) {
-		if ($percentage < 25) return 'progress-bar-danger';
-		if ($percentage < 50) return 'progress-bar-warning';
-		if ($percentage < 75) return 'progress-bar-success';
-		return 'progress-bar-info';
+class resources
+{
+	public $info   = [];
+	public $rows   = '';
+	public $t_head = '';
+
+	private $struct = [
+		'nat' => [
+			'chain' => '<th>CHAIN</th>',
+			'action' => '<th>ACTION</th>',
+			'protocol' => '<th>PROTO</th>',
+			'layer7-protocol' => '<th>L7_PROTO</th>',
+			'src-address' => '<th>SRC_ADDR</th>',
+			'dst-address' => '<th>DST_ADDR</th>',
+			'in-interface' => '<th>IN_INT</th>',
+			'out-interface' => '<th>OUT_INT</th>',
+			'src-port' => '<th>SRC_PORT</th>',
+			'dst-port' => '<th>DST_PORT</th>',
+			'bytes' => '<th>BTS</th>',
+			'packets' => '<th>PKTS</th>'
+		],
+
+		'filter' => [
+			'chain' => '<th>CHAIN</th>',
+			'action' => '<th>ACTION</th>',
+			'protocol' => '<th>PROTO</th>',
+			'layer7-protocol' => '<th>L7_PROTO</th>',
+			'src-address' => '<th>SRC_ADDR</th>',
+			'dst-address' => '<th>DST_ADDR</th>',
+			'in-interface' => '<th>IN_INT</th>',
+			'out-interface' => '<th>OUT_INT</th>',
+			'src-port' => '<th>SRC_PORT</th>',
+			'dst-port' => '<th>DST_PORT</th>',
+			'bytes' => '<th>BTS</th>',
+			'packets' => '<th>PKTS</th>'
+		],
+
+		'mangle' => [
+			'chain' => '<th>CHAIN</th>',
+			'action' => '<th>ACTION</th>',
+			'new-mss' => '<th>NEW_MSS</th>',
+			'passthrough' => '<th>PASSTHROUGH</th>',
+			'tcp-flags' => '<th>TCP_FLAGS</th>',
+			'protocol' => '<th>PROTO</th>',
+			'in-interface' => '<th>IN_INT</th>',
+			'tcp-mss' => '<th>TCP_MSS</th>',
+			'log' => '<th>LOG</th>',
+			'log-prefix' => '<th>LOG_PREFIX</th>',
+			'bytes' => '<th>BTS</th>',
+			'packets' => '<th>PKTS</th>',
+			'invalid' => '<th>INVALID</th>',
+			'dynamic' => '<th>DYNAMIC</th>',
+			'disabled' => '<th>DISABLED</th>'
+		],
+
+		'layer7-protocol' => [
+			'name' => '<th>NAME</th>',
+			'regexp' => '<th>REGEXP</th>'
+		],
+
+		'connection' => [
+			'protocol' => '<th>PROTO</th>',
+			'src-address' => '<th>SRC_ADDR</th>',
+			'dst-address' => '<th>DST_ADDR</th>',
+			'orig-bytes' => '<th>ORIG_BYTES</th>',
+			'orig-packets' => '<th>ORIG_PKTS</th>',
+			'reply-src-address' => '<th>REPLY_S_ADDR</th>',
+			'reply-dst-address' => '<th>REPLY_D_ADDR</th>',
+			'repl-bytes' => '<th>REPL_BYTES</th>',
+			'repl-packets' => '<th>REPL_PKTS</th>'
+		]
+	];
+
+
+	public function resources($table) {
+		$API = new routerosAPI();
+		$API->debug = false;
+
+		$config = json_decode(file_get_contents('./config.json'), true);
+
+		if (!$API->connect($config['hostname'], $config['username'], $config['password'])) {
+			die('API connection failed, aborting...');
+		}
+
+		$resources = $API->comm("/system/resource/print")['0'];
+
+		$this->info['platform'] = '<p><b>Platform:</b>' . $resources['platform'] . '</p>';
+		$this->info['model']    = '<p><b>Model:</b>' . $resources['board-name'] . '</p>';
+		$this->info['version']  = '<p><b>Version:</b>' . $resources['version'] . '</p>';
+		$this->info['cpu']      = '<p><b>CPU:</b>' . $resources['cpu'] . ' @ ' . $resources['cpu-frequency'] . 'MHz</p>';
+		$this->info['uptime']   = '<p><b>Uptime:</b>' . $resources['uptime'] . '</p>';
+
+		$this->info['cpu-load']       = '
+				<b class="progress-header">CPU (<font size="4em">' . $resources['cpu-load'] . '%</font>)</b>
+				<div class="progress progress-striped">
+					<div class="progress-bar ' . $this->getProgressClass($resources['cpu-load'], false) . '" style="width: ' . $resources['cpu-load'] . '%"></div>
+				</div>
+		';
+
+		$mem_perc = round(($resources['free-memory']/$resources['total-memory']) * 100);
+		$this->info['free-memory']    = '
+				<b class="progress-header">Free memory (<font size="4em">' . $mem_perc . '%</font>, ' . $resources['free-memory'] . 'KB)</b>
+				<div class="progress progress-striped">
+					<div class="progress-bar ' . $this->getProgressClass($mem_perc, true) . '" style="width: ' . $mem_perc . '%"></div>
+				</div>
+		';
+
+		$hdd_perc = round(($resources['free-hdd-space']/$resources['total-hdd-space']) * 100);
+		$this->info['free-hdd-space'] = '
+				<b class="progress-header">Free disk (<font size="4em">' . $hdd_perc . '%</font>, ' . $resources['free-hdd-space'] . 'KB)</b>
+				<div class="progress progress-striped">
+					<div class="progress-bar ' . $this->getProgressClass($hdd_perc, true) . '" style="width: ' . $hdd_perc . '%"></div>
+				</div>
+		';
+
+
+		if (!array_key_exists($table, $this->struct)) {
+			$this->t_head = '<script>document.querySelector(\'table\').style.display = \'none\';</script>';
+		} else {
+			$this->t_head = '<tr>';
+			foreach ($this->struct[$table] as $call => $head) {
+				$this->t_head += $head;
+			}
+			$this->t_head += '</tr>';
+
+			$results = $API->comm("/ip/firewall/$table/print");
+
+			foreach($results as $result) {
+				if ($table === 'nat' || $table === 'filter' || $table === 'mangle') {
+					$this->t_rows += '<tr title="" data-original-title="" type="button" data-container="body" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="';
+					$this->t_rows += $result['comment'] . '">';
+				} else {
+					$this->t_rows += '<tr>';
+				}
+
+				foreach ($this->struct[$table] as $call => $head)
+					$this->t_rows += '<td>' . $result[$call] . '</td>';
+
+				$this->t_rows += '</tr>';
+			}
+		}
+
+		$API->disconnect();
 	}
 
-	if ($percentage < 25) return 'progress-bar-info';
-	if ($percentage < 50) return 'progress-bar-success';
-	if ($percentage < 75) return 'progress-bar-warning';
-	return 'progress-bar-danger';
-}
 
-?>
+	public function getProgressClass($percentage, $rev) {
+		if ($rev) {
+			if ($percentage < 25) return 'progress-bar-danger';
+			if ($percentage < 50) return 'progress-bar-warning';
+			if ($percentage < 75) return 'progress-bar-success';
+			return 'progress-bar-info';
+		}
+
+		if ($percentage < 25) return 'progress-bar-info';
+		if ($percentage < 50) return 'progress-bar-success';
+		if ($percentage < 75) return 'progress-bar-warning';
+		return 'progress-bar-danger';
+	}
+}
